227ccd7ec8d245a4fc94833df12a782b96b060b4
lucasformento <lucasformento6256@gmail.com>
2024-10-24 19:32:59 -0300

    mejora fluent api

--- fluent_api.py
+++ fluent_api.py
@@ -4,19 +4,136 @@ class FluentQuery:
     def __init__(self):
         self.query = ""
 
-    # Método para realizar una consulta SELECT
     def traeme(self, *args):
         self.query += f"TRAEME {', '.join(args)} "
         return self
 
-    # Método para definir la tabla en la consulta
     def de_la_tabla(self, table_name):
         self.query += f"DE_LA_TABLA {table_name} "
         return self
 
-    # Método para agregar la condición WHERE
     def donde(self, condition):
         self.query += f"DONDE {condition} "
+        return self
+
+    def agrupando_por(self, *args):
+        self.query += f"AGRUPANDO_POR {', '.join(args)} "
+        return self
+    
+    def mezclando(self, table_name):    
+        self.query += f"MEZCLANDO {table_name} "
+        return self
+    
+    def en(self, condition):
+        self.query += f"EN {condition} "
+        return self
+    
+    def los_distintos(self, *args):
+        self.query += f"LOS_DISTINTOS {', '.join(args)} "
+        return self
+    
+    def contando(self, *args):
+        self.query += f"CONTANDO {', '.join(args)} "
+        return self
+    
+    def mete_en(self, table_name, *args):
+        self.query += f"METE_EN {table_name} ({', '.join(args)}) "
+        return self
+    
+    def los_valores(self, *args):
+        self.query += f"LOS_VALORES ({', '.join(args)}) "
+        return self
+    
+    def actualiza(self, table_name):
+        self.query += f"ACTUALIZA {table_name} "
+        return self
+    
+    def setea(self, condition):
+        self.query += f"SETEA {condition} "
+        return self
+    
+    def borra_de_la_tabla(self, table_name):
+        self.query += f"BORRA_DE_LA_TABLA {table_name} "
+        return self
+    
+    def ordena_por(self, *args):
+        self.query += f"ORDENA_POR {', '.join(args)} "
+        return self
+    
+    def como_mucho(self, limit):
+        self.query += f"COMO_MUCHO {limit} "
+        return self
+    
+    def where_del_group_by(self):
+        self.query += f"WHERE_DEL_GROUP_BY  "
+        return self
+    
+    def existe(self):
+        self.query += f"EXISTE "
+        return self
+    
+    def en_esto(self, condition):
+        self.query += f"EN_ESTO {condition} "
+        return self
+    
+    def entre(self, condition1, condition2):
+        self.query += f"ENTRE {condition1} AND {condition2} "
+        return self
+    
+    def parecido_a(self, condition):
+        self.query += f"PARECIDO_A {condition} "
+        return self
+    
+    def es_nulo(self):  
+        self.query += f"ES_NULO "
+        return self
+    
+    def cambia_la_tabla(self, table_name):
+        self.query += f"CAMBIA_LA_TABLA {table_name} "
+        return self
+    
+    def agrega_la_columna(self, column_name, data_type):
+        self.query += f"AGREGA_LA_COLUMNA {column_name} {data_type} "
+        return self
+    
+    def elimina_la_columna(self, column_name):
+        self.query += f"ELIMINA_LA_COLUMNA {column_name} "
+        return self
+    
+    def crea_la_tabla(self, table_name):
+        self.query += f"CREA_LA_TABLA {table_name} "
+        return self
+    
+    def tira_la_tabla(self, table_name):
+        self.query += f"TIRA_LA_TABLA {table_name} "
+        return self
+    
+    def por_defecto(self, value):
+        self.query += f"POR_DEFECTO {value} "
+        return self
+    
+    def unico(self):
+        self.query += f"UNICO "
+        return self
+    
+    def clave_prima(self):
+        self.query += f"CLAVE_PRIMA "
+        return self 
+
+    def clave_referente(self, column_name, table_name, column_name2):
+        self.query += f"CLAVE_REFERENTE ({column_name}) {table_name} ({column_name2}) "
+        return self
+    
+    def no_nulo(self):
+        self.query += f"NO_NULO "
+        return self
+    
+    def transforma_a(self, data_type):
+        self.query += f"TRANSFORMA_A {data_type} "
+        return self
+    
+    def y(self):
+        self.query += f"Y "
         return self
 
     # Método para insertar datos en la tabla 'usuarios'
--- main.py
+++ main.py
@@ -8,21 +8,20 @@ if __name__ == "__main__":
     # Crear la tabla en la base de datos si no existe
     create_table()
 
-    # DESCOMENTAR LOS SIGUIENTES EJEMPLOS PARA INSERTAR DATOS EN LA BASE DE DATOS. UNA VEZ QUE SE EJECUTEN, COMENTARLOS DE NUEVO PARA EVITAR DUPLICADOS
+    # DESCOMENTAR LOS SIGUIENTES EJEMPLOS PARA INSERTAR VALORES EN LA BASE DE DATOS. UNA VEZ QUE SE EJECUTEN, COMENTARLOS DE NUEVO PARA EVITAR DUPLICADOS
 
-    # También puedes usar la Fluent API para interactuar con la base de datos
     # fluent = FluentQuery()
-    # fluent.insert_usuarios("Juan", 40)  # Inserta datos a la tabla 'usuarios'
-    # fluent.insert_usuarios("María", 30)
-    # fluent.insert_usuarios("Pedro", 25)        # Consulta todos los datos de la tabla 'usuarios'
+    # fluent.insert_usuarios("Juan", 30)  # Inserta datos a la tabla 'usuarios'
+    # fluent.insert_usuarios("María", 15)
+    # fluent.insert_usuarios("Pedro", 40)      
 
-    # fluent.insert_empleados(2000, "Gerente")  # Inserta datos a la tabla 'empleados'
-    # fluent.insert_empleados(1500, "Asistente")
-    # fluent.insert_empleados(1000, "Secretaria")
+    # fluent.insert_empleados(2000, "ingeniero")  # Inserta datos a la tabla 'empleados'
+    # fluent.insert_empleados(1500, "ingeniero")
+    # fluent.insert_empleados(1000, "secretaria")
 
-    # fluent.insert_clientes("Ana", "CDMX")  # Inserta datos a la tabla 'clientes'
-    # fluent.insert_clientes("Luis", "Guadalajara")
-    # fluent.insert_clientes("Sofía", "Monterrey")    
+    # fluent.insert_clientes("Ana", "Madrid")  # Inserta datos a la tabla 'clientes'
+    # fluent.insert_clientes("Luis", "Barcelona")
+    # fluent.insert_clientes("Sofía", "Barcelona")    
 
     # fluent.insert_pedidos(1)  # Inserta datos a la tabla 'pedidos'  
     # fluent.insert_pedidos(2)
@@ -31,12 +30,74 @@ if __name__ == "__main__":
     # fluent.insert_ventas("Laptop")  # Inserta datos a la tabla 'ventas'
     # fluent.insert_ventas("Smartphone")  
     # fluent.insert_ventas("Tablet")
+    # fluent.insert_ventas("Tablet")
+    # fluent.insert_ventas("Tablet")
+    # fluent.insert_ventas("Tablet")
+    # fluent.insert_ventas("Tablet")
+    # fluent.insert_ventas("Tablet")
+    
+    ######################################################################################################################
+    # CONSULTAS DE LA LETRA (DESCOMENTAR LA QUE SE QUIERA PROBAR Y COMENTAR LAS DEMÁS PARA EVITAR CONFLICTOS DE EJECUCIÓN)
+
+    # Consulta para obtener todos los datos de la tabla 'usuarios'
+    # consulta_usql_api= FluentQuery().traeme("TODO").de_la_tabla("usuarios").donde("edad > 25").build()
+    # consulta_a_sql = parse_input(consulta_usql_api)
+    # print(f'Consulta en SQL: {consulta_a_sql}')
+    # execute_query(consulta_a_sql)
+
+    # Consulta para obtener los nombres de los clientes de la ciudad de Monterrey
+    # consulta_usql_api1= FluentQuery().traeme().los_distintos("nombre").de_la_tabla("clientes").donde("ciudad = 'Madrid'").build()
+    # consulta_a_sql1 = parse_input(consulta_usql_api1)
+    # print(f'Consulta en SQL: {consulta_a_sql1}')
+    # execute_query(consulta_a_sql1)
+
+    # Consulta para insertar datos en la tabla 'usuarios'   
+    # consulta_usql_api2= FluentQuery().mete_en("usuarios", "nombre", "edad").los_valores("'Juan'", "25").build()
+    # consulta_a_sql2 = parse_input(consulta_usql_api2)
+    # print(f'Consulta en SQL: {consulta_a_sql2}')
+    # execute_query(consulta_a_sql2)  
+
+    # Consulta para actualizar el salario de los empleados con puesto 'Gerente'
+    # consulta_usql_api3= FluentQuery().actualiza("empleados").setea("salario = 3000").donde("puesto = 'ingeniero'").build()
+    # consulta_a_sql3 = parse_input(consulta_usql_api3)
+    # print(f'Consulta en SQL: {consulta_a_sql3}')
+    # execute_query(consulta_a_sql3)  
+
+    # Consulta para obtener todo de un join entre las tablas 'pedidos' y 'clientes'
+    # consulta_usql_api4= FluentQuery().traeme("TODO").de_la_tabla("pedidos").mezclando("clientes").en("pedidos.cliente_id= clientes.id").donde("clientes.ciudad = 'Barcelona'").build()
+    # consulta_a_sql4 = parse_input(consulta_usql_api4)
+    # print(f'Consulta en SQL: {consulta_a_sql4}')
+    # execute_query(consulta_a_sql4)
+
+    # Consulta para obtener la cantidad de registros agrupados por 'producto' en la tabla 'ventas'
+    # consulta_usql_api5= FluentQuery().traeme().contando("(TODO)").de_la_tabla("ventas").agrupando_por("producto").where_del_group_by().contando("(TODO) > 5").build()
+    # consulta_a_sql5 = parse_input(consulta_usql_api5)
+    # print(f'Consulta en SQL: {consulta_a_sql5}')
+    # execute_query(consulta_a_sql5)  
+
+    # Consulta para borrar los registros de la tabla 'usuarios' donde la edad esté entre 18 y 25
+    # consulta_usql_api6= FluentQuery().borra_de_la_tabla("usuarios").donde("edad").entre("18", "25").build()
+    # consulta_a_sql6 = parse_input(consulta_usql_api6)
+    # print(f'Consulta en SQL: {consulta_a_sql6}')
+    # execute_query(consulta_a_sql6)
+
+    # Consulta para agregar una columna 'direccion' a la tabla 'empleados'
+    # consulta_usql_api7= FluentQuery().cambia_la_tabla("empleados").agrega_la_columna("direccion", "VARCHAR(255)").build()
+    # consulta_a_sql7 = parse_input(consulta_usql_api7)
+    # print(f'Consulta en SQL: {consulta_a_sql7}')
+    # execute_query(consulta_a_sql7)
+
+    # Consulta para eliminar la columna 'direccion' de la tabla 'empleados'
+    # consulta_usql_api8= FluentQuery().cambia_la_tabla("empleados").elimina_la_columna("direccion").build()
+    # consulta_a_sql8 = parse_input(consulta_usql_api8)
+    # print(f'Consulta en SQL: {consulta_a_sql8}')
+    # execute_query(consulta_a_sql8)
     
     while True:
 
         # Ejemplo de consulta USQL que será traducida a SQL y ejecutada en la base de datos
         consulta_usql = (input("Ingrese la consulta USQL: "))
-        
+
         print("Lexing...")
         tokens = lex_input(consulta_usql)
         # for token in tokens:
